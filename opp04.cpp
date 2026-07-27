d2d72261e1cc6b0bb44858da2ab4a28f9ce6b574
hysdus-byte <hysdus@gmail.com>
2026-07-27 16:51:36 +0900

    Rename opp4.cpp to opp04.cpp

---
+++ opp04.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//
+//int main()
+//{
+//	char name[100];
+//	char lang[200];
+//	int age;
+//	std::cout << "이름은 무엇입니까?";
+//	std::cin >> name;
+//
+//	std::cout << "좋아하는 프로그래밍 언어는 무엇인가요?";
+//	std::cin >> lang;
+//
+//	std::cout << "나이는 몇살입니까?";
+//	std::cin >> age;
+//
+//	std::cout << "내 이름은 " << name << "입니다.\n";
+//	std::cout << "제일 좋아하는 언어는 " << lang << "입니다" << std::endl;
+//	std::cout << "제 나이는 " << age << "입니다.\n";
+//	return 0;
+//}
